3f91f38f0666f4b67015f3fb1cbd592ff50490db
drkwithlove <43723672+drkwithlove@users.noreply.github.com>
2025-05-23 18:54:22 +0500

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,3 @@
-import time
-import threading
-import requests
 from flask import Flask
 from threading import Thread
 
@@ -8,22 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "I'm alive!"
+    return "Умка работает 💖"
 
 def run():
     app.run(host='0.0.0.0', port=8080)
 
-def self_ping():
-    while True:
-        try:
-            requests.get("https://d9106bfc-15f4-47c3-9123-f002f49768f0-00-34luxqspzmv08.sisko.replit.dev/")
-        except Exception as e:
-            print(f"Self-ping error: {e}")
-        time.sleep(60)  # Пинг каждые 60 секунд
-
 def keep_alive():
     t = Thread(target=run)
     t.start()
-    ping_thread = threading.Thread(target=self_ping)
-    ping_thread.daemon = True
-    ping_thread.start()
